2241aaafb5be10f162d8be41e1a8581c16cd19eb
Dinhtai2002 <147929359+Dinhtai2002@users.noreply.github.com>
2023-10-14 21:09:21 +0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View} from 'react-native';
+import Resgiter from './src/pages/Resgiter';
+// import Login from './src/pages/Login';
+
+const App = () => {
+  return (
+    <View>
+      {/* <Login /> */}
+      <Resgiter />
+    </View>
+  );
+};
+
+export default App;
